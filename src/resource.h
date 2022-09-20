790c64b5d9ef3df7e41eb3d8a3a17af2e0ec05c3
Don Ho <don.h@free.fr>
2022-09-20 18:50:30 +0200

    WinGUp (for Notepad++) v5.2.4 release

--- src/resource.h
+++ src/resource.h
@@ -19,8 +19,8 @@
 
 #pragma once
 
-#define VERSION_VALUE L"5.23\0"
-#define VERSION_DIGITALVALUE 5, 2, 3, 0
+#define VERSION_VALUE L"5.24\0"
+#define VERSION_DIGITALVALUE 5, 2, 4, 0
 
 #define IDD_PROGRESS_DLG 1001
 #define IDD_PROXY_DLG 1002
